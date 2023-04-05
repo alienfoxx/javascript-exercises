a958560ab17d041fe56a1bcf16e0eb438d48cd34
alienfoxx <alienfoxx@gmail.com>
2023-04-05 00:11:03 +0000

    remove elment from Array using function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+const arrayStrings = str.split("");
+const reversingArray = arrayStrings.reverse();
+const joinArray = reversingArray.join("");
 
+return joinArray;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;

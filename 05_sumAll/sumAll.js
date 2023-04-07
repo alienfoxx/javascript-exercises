792c6a2da0b052d127cd6e2298caa7e31cfa8749
alienfoxx <alienfoxx@gmail.com>
2023-04-07 15:09:44 +0000

    sumAll.Js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+    return 'ERROR';
+}
+if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
+let sum =0;
+let start = Math.min(num1, num2);
+let end = Math.max(num1,num2);
+for(let i = start; i<=end;i++){
+    sum+= i;
+}
+return sum;
 };
 
 // Do not edit below this line

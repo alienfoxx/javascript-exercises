f4c808f3a74b300b3d394287a3c5177a7525912d
alienfoxx <alienfoxx@gmail.com>
2023-04-15 23:45:08 +0000

    Claclute In Js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
+const add = function(a,b) {
+  let sum = a+b;
+  return sum;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  let sub = a -b;
+  return sub;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum =0;
+  for(let i =0; i<arr.length;i++)
+    
+  sum += arr[i];
+  return sum;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let sum =1;
+  for(let i =0; i<arr.length;i++)
+  sum =sum* arr[i];
+  return sum;
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  let sum = a **b;
+  return sum;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n < 0){
+    return "number has to be positive."
+}
+
+//base case
+if(n == 0 || n == 1){
+    return 1;
+//recursive case
+}else{
+    return n * factorial(n-1);
+}
+
 };
 
 // Do not edit below this line
